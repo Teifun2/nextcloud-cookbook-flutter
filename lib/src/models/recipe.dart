d0c224f522f283bc24f1eceed4316e95870b8f59
Linard Schwendener <linardschwendener10@gmail.com>
2020-07-19 16:00:46 +0200

    added Recipe Model

--- lib/src/models/recipe.dart
+++ lib/src/models/recipe.dart
@@ -1,32 +1,24 @@
-import 'dart:convert';
-
 import 'package:equatable/equatable.dart';
 
-class RecipeShort extends Equatable {
+class Recipe extends Equatable {
   final int _recipeId;
   final String _name;
   final String _userId;
   final String _imageUrl;
+  final String _recipeCategory;
 
   int get recipeId => _recipeId;
   String get name => _name;
   String get userId => _userId;
   String get imageUrl => _imageUrl;
+  String get recipeCategory => _recipeCategory;
 
-  RecipeShort.fromJson(Map<String, dynamic> json) :
+  Recipe.formJson(Map<String, dynamic> json) :
     _recipeId = int.parse(json["recipe_id"]),
     _name = json["name"],
     _userId = json["user_id"],
-    _imageUrl = json["imageUrl"];
-
-
-  static List<RecipeShort> parseRecipesShort(String responseBody) {
-    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-
-    return parsed
-        .map<RecipeShort>((json) => RecipeShort.fromJson(json))
-        .toList();
-  }
+    _imageUrl = json["imageUrl"],
+    _recipeCategory = json["recipeCategory"];
 
   @override
   List<Object> get props => [_recipeId];
