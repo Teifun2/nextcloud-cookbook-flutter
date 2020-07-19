8b8beecedfe9168cc61100c6c2702b187f343a03
Linard Schwendener <linardschwendener10@gmail.com>
2020-07-19 16:41:46 +0200

    Made it a statefull widget

--- lib/src/screens/recipe_screen.dart
+++ lib/src/screens/recipe_screen.dart
@@ -2,10 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nextcloud_cookbook_flutter/src/models/recipe_short.dart';
 
-class RecipeScreen extends StatelessWidget {
+
+class RecipeScreen extends StatefulWidget {
   final RecipeShort recipeShort;
 
   const RecipeScreen({Key key, @required this.recipeShort}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => RecipeScreenState();
+}
+
+class RecipeScreenState extends State<RecipeScreen> {
+  RecipeShort recipeShort;
+
+  @override
+  void initState() {
+    recipeShort = widget.recipeShort;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
