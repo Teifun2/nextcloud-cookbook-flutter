f04f553fd69cf224cfa673e42d8dd8c7b38c73c8
Linard Schwendener <linardschwendener10@gmail.com>
2020-07-21 09:26:01 +0200

    Added Clickable Instructions

--- lib/src/screens/recipe_screen.dart
+++ lib/src/screens/recipe_screen.dart
@@ -50,93 +50,113 @@ class RecipeScreenState extends State<RecipeScreen> {
   }
 
   Widget _buildRecipeScreen(Recipe recipe) {
-    return ListView(
-      children: <Widget>[
-        Container(
-          child: Center(
-            child: AuthenticationCachedNetworkImage(
-              imagePath: recipe.imageUrl,
+    List<bool> instructionsDone =
+        List.filled(recipe.recipeInstructions.length, false);
+
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return ListView(
+          children: <Widget>[
+            Container(
+              child: Center(
+                child: AuthenticationCachedNetworkImage(
+                  imagePath: recipe.imageUrl,
+                  width: double.infinity,
+                  height: 200,
+                  boxFit: BoxFit.cover,
+                ),
+              ),
               width: double.infinity,
               height: 200,
-              boxFit: BoxFit.cover,
             ),
-          ),
-          width: double.infinity,
-          height: 200,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: ListView(
-            shrinkWrap: true,
-            physics: ClampingScrollPhysics(),
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 15.0),
-                child: Text(
-                  recipe.name,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  recipe.description,
-                  style: TextStyle(fontStyle: FontStyle.italic),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  "Ingredients",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                    recipe.recipeIngredient.fold("", (p, e) => p + e + "\n")),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  "Instructions",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.min,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
                 children: <Widget>[
-                  ListView.separated(
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Row(
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(15),
-                            margin: EdgeInsets.only(right: 15),
-                            child: Text("${index + 1}"),
-                            decoration: ShapeDecoration(
-                              shape: CircleBorder(
-                                  side: BorderSide(color: Colors.grey)),
-                              color: Colors.grey[300],
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(recipe.recipeInstructions[index]),
-                          ),
-                        ],
-                      );
-                    },
-                    separatorBuilder: (c, i) => SizedBox(height: 10),
-                    itemCount: recipe.recipeInstructions.length,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 15.0),
+                    child: Text(
+                      recipe.name,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      recipe.description,
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      "Ingredients",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(recipe.recipeIngredient
+                        .fold("", (p, e) => p + e + "\n")),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      "Instructions",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListView.separated(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                instructionsDone[index] =
+                                    !instructionsDone[index];
+                              });
+                            },
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  margin: EdgeInsets.only(right: 15, top: 10),
+                                  child: instructionsDone[index]
+                                      ? Icon(Icons.check)
+                                      : Center(child: Text("${index + 1}")),
+                                  decoration: ShapeDecoration(
+                                    shape: CircleBorder(
+                                        side: BorderSide(color: Colors.grey)),
+                                    color: Colors.grey[300],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Text(recipe.recipeInstructions[index]),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        separatorBuilder: (c, i) => SizedBox(height: 10),
+                        itemCount: recipe.recipeInstructions.length,
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        ),
-      ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
