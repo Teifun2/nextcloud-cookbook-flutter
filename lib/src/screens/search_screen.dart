c0015448ec36fec3e77c691c254262192092dcd0
Linard Schwendener <linardschwendener10@gmail.com>
2020-08-02 15:51:39 +0200

    Show image + Spacing

--- lib/src/screens/search_screen.dart
+++ lib/src/screens/search_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:nextcloud_cookbook_flutter/src/models/recipe_short.dart';
 import 'package:nextcloud_cookbook_flutter/src/screens/recipe_screen.dart';
 import 'package:nextcloud_cookbook_flutter/src/services/data_repository.dart';
+import 'package:nextcloud_cookbook_flutter/src/widget/authentication_cached_network_image.dart';
 
 class SearchScreen extends StatelessWidget {
   SearchScreen() {
@@ -19,11 +20,16 @@ class SearchScreen extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: SearchBar<RecipeShort>(
           minimumChars: 3,
+          mainAxisSpacing: 5,
           onCancelled: () => Navigator.pop(context),
           onSearch: (text) => DataRepository().searchRecipes(text),
           onItemFound: (RecipeShort item, int index) {
             return ListTile(
               title: Text(item.name),
+              trailing: Container(
+                child:
+                    AuthenticationCachedNetworkImage(imagePath: item.imageUrl),
+              ),
               onTap: () => Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
