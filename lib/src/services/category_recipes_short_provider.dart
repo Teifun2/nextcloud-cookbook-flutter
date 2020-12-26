c0b3dc5d81a3f54c787ec520ce925d7cabc095e2
Linard Schwendener <linardschwendener10@gmail.com>
2020-12-26 23:24:57 +0100

    Each Provider switched to authenticatedClient

--- lib/src/services/category_recipes_short_provider.dart
+++ lib/src/services/category_recipes_short_provider.dart
@@ -1,25 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_translate/flutter_translate.dart';
-import 'package:http/http.dart';
 import 'package:nextcloud_cookbook_flutter/src/models/app_authentication.dart';
 import 'package:nextcloud_cookbook_flutter/src/models/recipe_short.dart';
 import 'package:nextcloud_cookbook_flutter/src/services/user_repository.dart';
 
 class CategoryRecipesShortProvider {
-  Client client = Client();
-
   Future<List<RecipeShort>> fetchCategoryRecipesShort(String category) async {
+    Dio client = UserRepository().getAuthenticatedClient();
     AppAuthentication appAuthentication =
         UserRepository().getCurrentAppAuthentication();
 
     final response = await client.get(
       "${appAuthentication.server}/index.php/apps/cookbook/category/$category",
-      headers: {
-        "authorization": appAuthentication.basicAuth,
-      },
     );
 
     if (response.statusCode == 200) {
-      return RecipeShort.parseRecipesShort(response.body);
+      return RecipeShort.parseRecipesShort(response.data);
     } else {
       throw Exception(translate('recipe_list.errors.load_failed'));
     }
