d02e8fdd3aaa34f300b36ed1026223da1384adce
Linard Schwendener <linardschwendener10@gmail.com>
2021-01-16 16:56:00 +0100

    Added API_Version_Warning

---
+++ lib/src/widget/api_version_warning.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+import 'package:nextcloud_cookbook_flutter/src/services/user_repository.dart';
+import 'package:nextcloud_cookbook_flutter/src/services/version_provider.dart';
+
+class ApiVersionWarning extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ApiVersion apiVersion = UserRepository().versionProvider.getApiVersion();
+
+    Future.delayed(const Duration(milliseconds: 100), () {
+      if (apiVersion.loadFailureMessage.isNotEmpty) {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              translate(
+                "categories.errors.api_version_check_failed",
+                args: {"error_msg": apiVersion.loadFailureMessage},
+              ),
+            ),
+            backgroundColor: Colors.red,
+          ),
+        );
+      } else if (apiVersion.isVersionAboveConfirmed()) {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              translate(
+                "categories.errors.api_version_above_confirmed",
+                args: {
+                  "version": apiVersion.majorApiVersion.toString() +
+                      "." +
+                      apiVersion.minorApiVersion.toString()
+                },
+              ),
+            ),
+            backgroundColor: Colors.orange,
+          ),
+        );
+      }
+    });
+
+    return Container();
+  }
+}
