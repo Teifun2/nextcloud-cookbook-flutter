908f53773bda140d381c1d8234ad5f62d07154cd
Linard Schwendener <linardschwendener10@gmail.com>
2022-09-07 17:44:35 +0200

    #141 Filter all categories and empty categories form category search

--- lib/src/services/category_search_provider.dart
+++ lib/src/services/category_search_provider.dart
@@ -1,11 +1,13 @@
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:nextcloud_cookbook_flutter/src/models/category.dart';
 
 class CategorySearchProvider {
   List<String> categoryNames = [];
   bool categoriesLoaded = false;
+  static String categoryAll = translate('categories.all_categories');
 
   void updateCategoryNames(List<Category> categories) {
-    categoryNames = categories.map((e) => e.name).toList();
+    categoryNames = categories.map((e) => e.name).where((element) => element != categoryAll && element != '*').toList();
     categoriesLoaded = true;
   }
 
