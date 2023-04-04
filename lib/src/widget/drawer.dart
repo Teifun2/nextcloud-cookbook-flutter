bf535daeb7b94f9d118219f786d76ad56386a731
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2023-04-04 17:00:16 +0200

    redesign drawer

--- lib/src/widget/drawer.dart
+++ lib/src/widget/drawer.dart
@@ -5,6 +5,7 @@ import 'package:nextcloud_cookbook_flutter/src/blocs/authentication/authenticati
 import 'package:nextcloud_cookbook_flutter/src/screens/my_settings_screen.dart';
 import 'package:nextcloud_cookbook_flutter/src/screens/recipe_import_screen.dart';
 import 'package:nextcloud_cookbook_flutter/src/screens/timer_screen.dart';
+import 'package:nextcloud_cookbook_flutter/src/widget/drawer_item.dart';
 import 'package:nextcloud_cookbook_flutter/src/widget/user_image.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -16,21 +17,17 @@ class MainDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        // Important: Remove any padding from the ListView.
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
             decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
+              color: Theme.of(context).colorScheme.primaryContainer,
             ),
             child: const UserImage(),
           ),
-          ListTile(
-            trailing: Icon(
-              Icons.alarm_add_outlined,
-              semanticLabel: translate('timer.title'),
-            ),
-            title: Text(translate('timer.title')),
+          DrawerItem(
+            icon: Icons.alarm_add_outlined,
+            title: translate('timer.title'),
             onTap: () {
               Navigator.pop(context);
               Navigator.push(
@@ -41,12 +38,9 @@ class MainDrawer extends StatelessWidget {
               );
             },
           ),
-          ListTile(
-            trailing: Icon(
-              Icons.cloud_download_outlined,
-              semanticLabel: translate('categories.drawer.import'),
-            ),
-            title: Text(translate('categories.drawer.import')),
+          DrawerItem(
+            icon: Icons.cloud_download_outlined,
+            title: translate('categories.drawer.import'),
             onTap: () {
               Navigator.pop(context);
               Navigator.push(
@@ -57,12 +51,9 @@ class MainDrawer extends StatelessWidget {
               );
             },
           ),
-          ListTile(
-            trailing: Icon(
-              Icons.settings_outlined,
-              semanticLabel: translate('categories.drawer.settings'),
-            ),
-            title: Text(translate('categories.drawer.settings')),
+          DrawerItem(
+            icon: Icons.settings_outlined,
+            title: translate('categories.drawer.settings'),
             onTap: () async {
               await Navigator.push(
                 context,
@@ -72,12 +63,9 @@ class MainDrawer extends StatelessWidget {
               );
             },
           ),
-          ListTile(
-            trailing: Icon(
-              Icons.exit_to_app_outlined,
-              semanticLabel: translate('app_bar.logout'),
-            ),
-            title: Text(translate('app_bar.logout')),
+          DrawerItem(
+            icon: Icons.exit_to_app_outlined,
+            title: translate('app_bar.logout'),
             onTap: () {
               BlocProvider.of<AuthenticationBloc>(context)
                   .add(const LoggedOut());
