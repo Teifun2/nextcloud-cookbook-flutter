39046f8df1c1c434c44759222b322d21b41ccc1a
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2023-04-16 20:32:45 +0200

    rework loading screen
    - center

--- lib/src/screens/loading_screen.dart
+++ lib/src/screens/loading_screen.dart
@@ -4,39 +4,39 @@ import 'package:flutter_translate/flutter_translate.dart';
 import 'package:nextcloud_cookbook_flutter/src/blocs/authentication/authentication_bloc.dart';
 
 class LoadingErrorScreen extends StatelessWidget {
-  const LoadingErrorScreen({super.key});
+  const LoadingErrorScreen({required this.message, super.key});
+
+  final String message;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-        builder: (context, authenticationState) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(authenticationState.error!),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<AuthenticationBloc>(context)
-                        .add(const AppStarted());
-                  },
-                  child: Text(translate("login.retry")),
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    BlocProvider.of<AuthenticationBloc>(context)
-                        .add(const LoggedOut());
-                  },
-                  child: Text(translate("login.reset")),
-                ),
-              ],
-            ),
-          );
-        },
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(message),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  BlocProvider.of<AuthenticationBloc>(context)
+                      .add(const AppStarted());
+                },
+                child: Text(translate("login.retry")),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  BlocProvider.of<AuthenticationBloc>(context)
+                      .add(const LoggedOut());
+                },
+                child: Text(translate("login.reset")),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
